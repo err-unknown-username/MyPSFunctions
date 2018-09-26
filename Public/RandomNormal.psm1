f276ee4c587d164def19ff4aa9cd5eb88fd96a9b
David Whitehead <dwhitehead@samarskite.com>
2018-09-26 16:19:06 -0700

    first commit

---
+++ Public/RandomNormal.psm1
@@ -0,0 +1,18 @@
+﻿function Get-RandomNormal
+{
+    [CmdletBinding()]
+    Param ( 
+        [Parameter(Mandatory=$true)]
+        [double]$Mean, 
+
+        [Parameter(Mandatory=$true)]
+        [double]$StandardDeviation 
+    )
+ 
+    $RandomNormal = $Mean + $StandardDeviation * [math]::Sqrt( -2 * [math]::Log( ( Get-Random -Minimum 0.0 -Maximum 1.0 ) ) ) * [math]::Cos( 2 * [math]::PI * ( Get-Random -Minimum 0.0 -Maximum 1.0 ) )
+ 
+    Write-Output $RandomNormal
+}
+Export-ModuleMember -Function Get-RandomNormal
+
+#Get-RandomNormal 100 30
